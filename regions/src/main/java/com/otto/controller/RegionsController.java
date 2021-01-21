fd3a0b2a3ddc307ed62c33fac60019207a5bf41b
Pooja1993 <psnavalgund@gmail.com>
2021-01-21 01:41:32 +0100

    Added validation for regions

--- regions/src/main/java/com/otto/controller/RegionsController.java
+++ regions/src/main/java/com/otto/controller/RegionsController.java
@@ -1,5 +1,8 @@
 package com.otto.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +18,17 @@ public class RegionsController {
 
 	@GetMapping("/regions")
 	public String getRegions(@RequestParam(name="region", defaultValue="ALL", required=false) String region) {
-        return regionsService.getRegions(region);
+		List<String> allowedValues = getAllowedValuesFromDB();
+	     if(allowedValues.contains(region)){
+	 		return regionsService.getRegions(region);
+	     }
+	     return "Enter valid region(EU, US, AP, CN, SA, AF, CA or ALL)";
+	}
+	
+	private List<String> getAllowedValuesFromDB(){
+		String[] av = {"EU","US","AP","CN","SA","AF","CA","ALL"};
+	    List<String> allowedValues = Arrays.asList(av);
+	    return allowedValues;
 	}
 
 }
