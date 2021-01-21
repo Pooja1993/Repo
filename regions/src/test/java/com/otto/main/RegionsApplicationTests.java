549ea06099b61db8325ec542b100109cf114ea1f
Pooja1993 <psnavalgund@gmail.com>
2021-01-21 01:40:37 +0100

    Added new test cases

--- regions/src/test/java/com/otto/main/RegionsApplicationTests.java
+++ regions/src/test/java/com/otto/main/RegionsApplicationTests.java
@@ -3,22 +3,19 @@ package com.otto.main;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.web.servlet.MockMvc;
 
 import com.otto.controller.RegionsController;
 import com.otto.service.RegionsService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+
 @SpringBootTest
 class RegionsApplicationTests {
 	
 	@Autowired
 	private RegionsService service;
-	/*
-	@Autowired
-    private MockMvc mvc;*/
 	
 	@Test
 	void contextLoads() {
@@ -28,28 +25,32 @@ class RegionsApplicationTests {
 	@Test
 	public void readsFromURL() {
 	    String found = service.readURL();
-	     assertThat(found.contains("region"));
+	    assertThat(found.contains("region"));
 	}
 	
-	/*@Test
-	public void something() {
-		String region = "CN";
-		String found = service.getRegions(region);
-		System.out.println(found);
-		assertThat(found.contains(region.toLowerCase()));
-	}*/
-	/*
 	@Test
-	public void givenEmployees_whenGetEmployees_thenStatus200()
-	  throws Exception {
-
-
-	    mvc.perform(get("/api/employees")
-	      .contentType(MediaType.APPLICATION_JSON))
-	      .andExpect(status().isOk())
-	      .andExpect(content()
-	      .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-	      .andExpect(jsonPath("$[0].name", is("bob")));
-	}*/
+    public void errorMessageOnInvalidInput() {
+		String region = "CP";
+        RegionsController controller = new RegionsController();
+        String result = controller.getRegions(region);
+        assertThat(result.contains("Enter valid region"));
+    }
+	
+	@Test
+	public void containsRegionIfValid() {
+		String region = "EU";
+		String[] av = {"us","ap","cn","sa","af","ca"};
+		String found = service.getRegions(region);
+	    assertThat(found.contains(region.toLowerCase()) 
+	    		&& !Arrays.stream(av).anyMatch(found::contains));
+	}
+	
+	@Test
+	public void containsAllRegionsWithAllParam() {
+		String region = "ALL";
+		String[] av = {"eu","us","ap","cn","sa","af","ca"};
+		String found = service.getRegions(region);
+	    assertThat(Arrays.stream(av).allMatch(found::contains));
+	}
 
 }
