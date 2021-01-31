2b25bed984e2c336cb4512e2c5e037e84a8090ed
Pooja1993 <psnavalgund@gmail.com>
2021-01-31 20:21:07 +0100

    Update RegionsController.java

--- regions/src/main/java/com/otto/controller/RegionsController.java
+++ regions/src/main/java/com/otto/controller/RegionsController.java
@@ -5,18 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.otto.service.RegionsService;
-import com.otto.service.ValidationService;
+import com.otto.service.RegionsServiceImpl;
 
 @RestController
 public class RegionsController {
 	
 	@Autowired
-	RegionsService regionsService;
+	RegionsServiceImpl regionsService;
 
 	@GetMapping("/regions")
 	public String getRegions(@RequestParam(name="region", defaultValue="ALL", required=false) String region) {
-	     if(ValidationService.getAllowedValues().contains(region)){
+	     if(regionsService.getAllowedValues().contains(region)){
 	 		return regionsService.getRegions(region);
 	     }
 	     return "Enter valid region(EU, US, AP, CN, SA, AF, CA or ALL)";
